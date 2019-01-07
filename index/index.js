2d6801ac6d51d6e8a71c9316a6166bcf575978f7
JulianKov <yulian.kov@gmail.com>
2019-01-07 22:20:01 +0300

    game

---
+++ index/index.js
@@ -0,0 +1,10 @@
+const slides = document.querySelectorAll('#slides .slide');
+let currentSlide = 0;
+
+function nextSlide() {
+  slides[currentSlide].className = 'slide';
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].className = 'slide showing';
+}
+
+setInterval(nextSlide, 2000);
